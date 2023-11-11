702fecf4c8d389ef623e9b74c85fcd82f7aa79e7
RalfDuli <ralfduli24@gmail.com>
2023-11-11 12:45:24 +0000

    Endpoint broadcasts data to itself

--- client.py
+++ client.py
@@ -1,53 +1,60 @@
 import socket
 import os
-import subprocess
-import json
-import docker
 import sys
+import random
+import threading
+import time
+import struct
 
 class Endpoint:
-    def __init__(self) -> None:
+
+    def __init__(self, argv) -> None:
+        self.buffersize = 50000
         self.port = 5000
         self.container_id = self.get_container_id()
+        self.connected_network = argv[0]
+        self.id = random.randint(0,10000)
+        self.msg = f"Message from endpoint {self.id}"
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('0.0.0.0' , self.port))
 
-
-    """ def get_container_ip(self, container_id):
-        try:
-            command = f"docker inspect --format='{{.NetworkSettings.IPAddress}}' {container_id}"
-            result = subprocess.run(command, shell=True, capture_output=True, text=True)
-            ip_address = result.stdout.strip()  # Remove newline characters
-            return ip_address
-
-        except subprocess.CalledProcessError as e:
-            print(f"Error executing Docker command: {e}")
-            return None
-
-
-
-    def get_networks(self, container_id):
-        try:
-            client = docker.DockerClient(base_url='unix://var/run/docker.sock')  # Manually specifying the Unix socket
-            container = client.containers.get(container_id)
-            networks = container.attrs['NetworkSettings']['Networks']
-            return networks.keys()
-        except docker.errors.NotFound as e:
-            print(f"Container not found: {e}")
-            return None
-        except docker.errors.APIError as e:
-            print(f"API error: {e}")
-            return None """
+        print(f'Endpoint {self.id} initialised!')
 
     def get_container_id(self):
         container_id = os.getenv("HOSTNAME")
         return container_id
     
+    def broadcast(self):
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        network = self.connected_network
+        network_to_send_to = network + '255'
+        print(f'network to send to: {network_to_send_to}')
+        msg_to_send = struct.pack('i', self.id) + self.msg.encode()
+        self.sock.sendto(msg_to_send, (network_to_send_to, self.port))
+
+    def receive(self):
+        print('receive thread started')
+        while True:
+            received_packet, _ = self.sock.recvfrom(self.buffersize)
+            header_size = struct.calcsize('i')
+            sender_id = (struct.unpack('i',  received_packet[:header_size]))[0]
+            contents_of_msg = received_packet[header_size:].decode()
+            print(contents_of_msg)
+    
 
 def main(argv):
-    e = Endpoint()
-    print('hi')
+    e = Endpoint(argv)
+    print(e.connected_network)
+    
+    sending_thread = threading.Thread(target=e.broadcast)
+    receiving_thread = threading.Thread(target=e.receive)
 
-    while True:
-        pass
+    receiving_thread.start()
+    time.sleep(0.02)
+    sending_thread.start()
+
+    sending_thread.join()
+    receiving_thread.join()
 
 if __name__ == '__main__':
     main(sys.argv[1:])

05dbd6d48c4f001bc76c49ca9ae63efb61da4e9c
RalfDuli <ralfduli24@gmail.com>
2023-11-22 22:06:42 +0000

    Client receives data once

--- client.py
+++ client.py
@@ -1,9 +1,8 @@
 import socket
 import sys
 import random
-import threading
-import time
 import struct
+import time
 
 class Endpoint:
 
@@ -11,18 +10,28 @@ class Endpoint:
         self.buffersize = 50000
         self.port = 5000
         self.connected_network = connected_network[0]
-        self.id = random.randint(0,10000)
+        self.id = self.generate_random_number()
         self.msg = f"Message from endpoint {self.id}"
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(('0.0.0.0' , self.port))
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.sock.bind(('0.0.0.0', self.port))
+        self.endpoint_ip = socket.gethostbyname(socket.gethostname())
+        self.received_sender_ids = set()
 
         print(f'Endpoint {self.id} initialised!')
     
+    def generate_random_number(self):
+        return random.randint(0,10000) + random.randint(0,10000) + random.randint(0,10000)
+    
     def broadcast(self):
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         network = self.connected_network
+
+        print('connected network:',self.connected_network)
+        print('Sending message to network.')
+
         network_to_send_to = network + '255'
         print(f'network to send to: {network_to_send_to}')
+        
         msg_to_send = struct.pack('i', self.id) + self.msg.encode()
         self.sock.sendto(msg_to_send, (network_to_send_to, self.port))
 
@@ -30,25 +39,19 @@ class Endpoint:
         while True:
             received_packet, _ = self.sock.recvfrom(self.buffersize)
             header_size = struct.calcsize('i')
-            sender_id = (struct.unpack('i',  received_packet[:header_size]))[0]
-            if sender_id != self.id:
+            sender_id= (struct.unpack('i',  received_packet[:header_size]))[0]
+            if sender_id != self.id and sender_id not in self.received_sender_ids:
                 contents_of_msg = received_packet[header_size:].decode()
                 print(contents_of_msg)
-    
+                self.received_sender_ids.add(sender_id)
+
 
 def main(argv):
     e = Endpoint(argv)
     print(e.connected_network)
-    
-    sending_thread = threading.Thread(target=e.broadcast)
-    receiving_thread = threading.Thread(target=e.receive)
-
-    receiving_thread.start()
-    time.sleep(0.02)
-    sending_thread.start()
-
-    sending_thread.join()
-    receiving_thread.join()
+    time.sleep(random.uniform(0,5))
+    e.broadcast()
+    e.receive()
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv[1:])  
--- server.py
+++ server.py
@@ -11,14 +11,36 @@ class Router:
         self.port = 5000
         self.connected_networks = connected_networks
         #self.router_ip = socket.gethostbyname(socket.gethostname())
-        self.id = random.randint(0,10000)
         self.packet_received = ()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0' , self.port))
         self.ip_address = self.sock.getsockname()[0]
+        #self.received_sender_ids = set()
+        #self.ip_addresses = self.get_ip_addresses()
 
         print(f'Router initialised!')
-        print('IP ADDRESS',self.ip_address)
+        #print('IP ADDRESS',self.ip_address)
+
+
+    # def get_ip_addresses(self):
+    #     addresses = {}
+        
+    #     # Get address information for all available network interfaces
+    #     for res in socket.getaddrinfo(socket.gethostname(), None):
+    #         af, socktype, proto, canonname, sockaddr = res
+
+    #         # Filter out IPv4 and IPv6 addresses
+    #         if af == socket.AF_INET or af == socket.AF_INET6:
+    #             interface_name = sockaddr[0]
+    #             ip_address = sockaddr[0]
+
+    #             # Store the IP address in the dictionary
+    #             if interface_name not in addresses:
+    #                 addresses[interface_name] = []
+    #             addresses[interface_name].append(ip_address)
+    #     addresses = list(addresses.keys())
+    #     print('ADDRESSES', addresses, type(addresses))
+    #     return addresses
 
 
     
@@ -26,40 +48,38 @@ class Router:
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         msg_to_relay, received_msg_ip_address = self.packet_received
-
+        
+        header_size = struct.calcsize('i')
+        sender_id = (struct.unpack('i',  msg_to_relay[:header_size]))[0]
+        
         received_msg_ip_address = ipaddress.ip_address(received_msg_ip_address[0])
         print(received_msg_ip_address)
 
-        #for network in self.connected_networks:
-        #    network_to_broadcast_to = network + '255'
-        #    self.sock.sendto(msg_to_relay, (network_to_broadcast_to, self.port))
-        
+        # if sender_id in self.received_sender_ids:
+        #     print('DEBUG: Sender ID =', sender_id)
+        #     return
+        #print(f'Received {msg_to_relay.decode()}')
         for network in self.connected_networks:
             network = network + '0/24'
-
             for ip in ipaddress.IPv4Network(network):
-                if ip != received_msg_ip_address and ip != self.ip_address:
-                    
-                    self.sock.sendto(msg_to_relay, (str(ip), self.port))
-                    #print(f"Message sent to {broadcast_address}")
-                else:
-                    print('very nice! IP =', ip)
+                self.sock.sendto(msg_to_relay, (str(ip), self.port))
+
 
 
     def listen(self):
         while True:
             received_packet = self.sock.recvfrom(self.buffersize)
-            print(received_packet)
             self.packet_received = received_packet
 
-            if received_packet[1][0] == self.ip_address:
-                continue
-
             header_size = struct.calcsize('i')
+            sender_id = (struct.unpack('i',  received_packet[0][:header_size]))[0]
+
             contents_of_msg = received_packet[0][header_size:].decode()
-            print(f'Received {contents_of_msg}')
 
             self.broadcast()
+
+            #self.received_sender_ids.add(sender_id)
+            #print('DEBUG: set =',self.received_sender_ids)
     
 
 def main(argv):

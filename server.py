80bda329a4e59bfe0b51f414416732466ef10abd
RalfDuli <ralfduli24@gmail.com>
2023-11-22 11:46:15 +0000

    Bug fixes

--- server.py
+++ server.py
@@ -10,12 +10,15 @@ class Router:
         self.buffersize = 50000
         self.port = 5000
         self.connected_networks = connected_networks
+        #self.router_ip = socket.gethostbyname(socket.gethostname())
         self.id = random.randint(0,10000)
         self.packet_received = ()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0' , self.port))
+        self.ip_address = self.sock.getsockname()[0]
 
         print(f'Router initialised!')
+        print('IP ADDRESS',self.ip_address)
 
 
     
@@ -24,22 +27,33 @@ class Router:
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         msg_to_relay, received_msg_ip_address = self.packet_received
 
-        received_msg_ip_address = received_msg_ip_address[0]
+        received_msg_ip_address = ipaddress.ip_address(received_msg_ip_address[0])
+        print(received_msg_ip_address)
 
+        #for network in self.connected_networks:
+        #    network_to_broadcast_to = network + '255'
+        #    self.sock.sendto(msg_to_relay, (network_to_broadcast_to, self.port))
+        
         for network in self.connected_networks:
             network = network + '0/24'
 
             for ip in ipaddress.IPv4Network(network):
-                if str(ip) != received_msg_ip_address:
-                    broadcast_address = str(ip)
+                if ip != received_msg_ip_address and ip != self.ip_address:
                     
-                    self.sock.sendto(msg_to_relay, (str(ip), self.port))  # Change the port number as needed
-                    print(f"Message sent to {broadcast_address}")
+                    self.sock.sendto(msg_to_relay, (str(ip), self.port))
+                    #print(f"Message sent to {broadcast_address}")
+                else:
+                    print('very nice! IP =', ip)
 
-    def receive(self):
+
+    def listen(self):
         while True:
             received_packet = self.sock.recvfrom(self.buffersize)
+            print(received_packet)
             self.packet_received = received_packet
+
+            if received_packet[1][0] == self.ip_address:
+                continue
 
             header_size = struct.calcsize('i')
             contents_of_msg = received_packet[0][header_size:].decode()
@@ -51,7 +65,7 @@ class Router:
 def main(argv):
     e = Router(argv)
     print(f'Connected networks to the router: {e.connected_networks}')
-    e.receive()
+    e.listen()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
